7628f2dceef2cb3363ae069ce4d2fd01492f2d37
gotyguyg <101912048+gotyguyg@users.noreply.github.com>
2022-06-03 09:13:54 +0000

    Update -worker.js

--- -worker.js
+++ -worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'sfrwedw3.herokuapp.com'
+      url.hostname = 'rtgerfwed5.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
